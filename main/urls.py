173fcb2f351360c8f112f0a538cfa65900bc9f0e
muzakiahmdz <muzaki.ahmad@ui.ac.id>
2023-09-27 13:59:21 +0700

    tutor 4 donn

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, register, login_user,logout_user ,create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, register, login_user,logout_user, edit_product, delete_product ,create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
 
 
 
@@ -16,6 +16,8 @@ urlpatterns = [
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'), # sesuaikan dengan nama fungsi yang dibuat
 
     
 
