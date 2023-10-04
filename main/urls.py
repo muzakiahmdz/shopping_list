dec8513a44c2535cd40633d3e1603f815b81b77e
muzakiahmdz <muzaki.ahmad@ui.ac.id>
2023-10-04 14:07:51 +0700

    tutorial 5 donn

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, register, login_user,logout_user, edit_product, delete_product ,create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, register, login_user,logout_user, edit_product, delete_product ,create_product, show_xml, show_json, show_json_by_id ,show_xml_by_id, get_product_json, add_product_ajax 
 
 
 
@@ -18,7 +18,8 @@ urlpatterns = [
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'), # sesuaikan dengan nama fungsi yang dibuat
-
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
     
 
 ]
